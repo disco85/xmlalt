1a1ad29b7c43d71090d68867b1796a918dcb306f
John Doe <937310@proton.me>
2026-05-06 18:41:48 +0300

    Better model; started classes

--- model.lisp
+++ model.lisp
@@ -21,7 +21,7 @@
    (prefix :initform nil :type prefix)))
 
 
-(defclass path ()
+(defclass dir ()
   ((names :type cons
           :documentation "Non-empty list of NAME")))
 
@@ -29,8 +29,34 @@
   (length (path-names)))
 
 
-(defclass construct ()
-  ((path :type path
-         :documentation "A construct is located at some PATH reflecting it nesting")
+(defclass content ()
+  ((value :type string :initform nil)))
+
+
+(defclass node ()
+  ((dir :type dir
+         :documentation "A construct is located at some DIR reflecting it nesting")
    (children :initform nil :type list
-             :documentation "Refers zero or more children of its own type CONSTRUCT")))
+             :documentation "Refers zero or more children of its own type CONSTRUCT")
+   (open-by :type string :initform "<" :reader node-open-by)
+   (close-by :type string :initform ">" :reader node-open-by)))
+
+
+(defclass attribute ()
+  ((name :type name)
+   (value :type content)))
+
+
+(defclass text (node)
+  ((open-by :initform "")
+   (close-by :initform "")
+   (conent :type content)))
+
+
+(defclass pi (node)
+  ((open-by :initform "<?")
+   (close-by :initform "?>")
+   (content :type content)))
+
+
+;; TODO
