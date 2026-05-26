cb0fc50b9d873bba54d15030e222d456f4e40b9b
John Doe <937310@proton.me>
2026-05-26 19:57:45 +0300

    More on CLI

--- xmlalt.lisp
+++ xmlalt.lisp
@@ -1,52 +1,59 @@
 (in-package :xmlalt)
 
+;; Supported formats:
+(defparameter +formats+ '(:cmdfmt :regfmt :xmlfmt))
+
 
 (defun cli-main-cmd-handler (cmd)
   (declare (ignorable cmd))
-  (let* ((dry-run (clingon:getopt cmd :dry))
-         (verbose-run (clingon:getopt cmd :verbose))
-         (prepend-with (clingon:getopt cmd :prepend))
-         (input-lines (read-stdin-lines))
-         (parsed-lines (acl2::parse-tree-output input-lines))
-         (classified-lines (acl2::classify-tree-output parsed-lines))
-         (prepended-classified-lines
-           (if prepend-with
-               (acl2::prepend-classified-tree-output prepend-with classified-lines)
-               classified-lines)))
-    ;; (format t "~{~A~%~}~%" prepended-classified-lines)
-    (ensure-classified-tree-output prepended-classified-lines :immediately dry-run verbose-run)))
+  (let* ((in (clingon:getopt cmd :in))
+         (out (clingon:getopt cmd :out))
+         (from (clingon:getopt cmd :from))
+         (to (clingon:getopt cmd :to)))
+    (format t "RUN: ~A ~A ~A ~A~%" in out from to)
+    t))
 
 
-
+;; TODO add options for delimiters
 (defun cli-main-cmd-opts ()
   (list
    (clingon:make-option
     :string
-    :description "Prepend paths with common directory-prefix"
-    :short-name #\p
-    :long-name "prepend"
-    :key :prepend)
+    :description "Input file [missing option for stdin]"
+    :short-name #\i
+    :long-name "in"
+    :key :in)
    (clingon:make-option
-    :flag
-    :description "Dry run"
-    :short-name #\d
-    :long-name "dry"
-    :key :dry)
+    :string
+    :description "Output file [missing option for stdout]"
+    :short-name #\o
+    :long-name "out"
+    :key :out)
    (clingon:make-option
-    :flag
-    :description "Verbose run"
-    :short-name #\v
-    :long-name "verbose"
-    :key :verbose)))
+    :choice
+    :description "From format"
+    :short-name #\f
+    :long-name "from"
+    :key :from
+    :initial-value :xmlfmt
+    :items +formats+)
+   (clingon:make-option
+    :choice
+    :description "To format"
+    :short-name #\t
+    :long-name "to"
+    :key :to
+    :initial-value :cmdfmt
+    :items +formats+)))
 
 
 (defun cli-main-cmd ()
   (clingon:make-command
    :name "xmlalt"
    :description "XML serial alternative formats converter"
-   :usage "-i,--in FILE|- -o,--out FILE|- -f,--format CMD,REG"
-   :examples '(("Do X" . "xmlalt -x")
-               ("Do Y" . "xmlalt -y"))
+   :usage "[-i|--in FILE] [-o|--out FILE] -f|--from CMDFMT|REGFMT|XMLFMT -t|--to CMDFMT|REGFMT|XMLFMT"
+   :examples '(("Convert XML to some serial format from stdin to stdout" . "cat a.xml|xmlalt -f XMLFMT -t CMDFMT")
+               ("Convert a serial format from a file to XML to a file" . "xmlalt -i a.reg -f REGFMT -t XMLFMT -o a.xml"))
    :version "0.1.0"
    :authors '("John Doe <john.doe@example.org>")
    :license "GPL-3.0-or-later"
