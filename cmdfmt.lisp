5dccee7d836ff37c4f9daff5354c2dedcb9396ff
John Doe <937310@proton.me>
2026-06-19 20:47:16 +0300

    Stop to serialize bogus-container for CMDFMT

--- cmdfmt.lisp
+++ cmdfmt.lisp
@@ -7,7 +7,12 @@
   "Serializes MODEL:DOC object"
   ;;(serialize-dtd doc stream)
   (with-truly root (model:get-doc-root doc)
-    (serialize-nodes doc root stream)))
+    (serialize-nodes
+     doc
+     root
+     stream
+     ;; ROOT IS bogus-container, but we check it anywhere (what if the code will be changed?):
+     (null (model:get-node-parent root)))))
 
 
 (defun serialize-dtd (doc dtd stream)
@@ -119,47 +124,51 @@
                  (cdr pair)))))
 
 
-(defun serialize-nodes (doc node stream)
+(defun serialize-nodes (doc node stream &optional bogus-container)
   "Serializes XML node"
   (typecase node
-    (model:dtd (serialize-dtd doc node stream))
+    (model:dtd
+       (serialize-dtd doc node stream))
     (model:elem
-     (format stream ".EL ~A ~A~%"
-             (model:get-elem-children-num node)
-             (model:calc-node-dir node :join-by *dir-delim*))
-     (with-truly namespace-uri (model:get-elem-namespace-uri node)
-       (format stream ".EL.NS ~A~%" (model:write-uri namespace-uri)))
-     (with-truly local-name (model:get-elem-local-name node)
-       (format stream ".EL.LOC.NAME ~A~%" local-name))
-     (with-truly qname (model:get-elem-qname node)
-       (format stream ".EL.QNAME ~A~%" qname))
-     (with-truly prefix-mappings (model:get-elem-prefix-mappings node)
-       (serialize-prefix-mappings doc node prefix-mappings stream))
-     (with-truly attributes (model:get-elem-attributes node)
-       (serialize-elem-attributes doc attributes stream))
-     (with-truly children (model:get-elem-children node)
-       (dolist (child children)
-         (serialize-nodes doc child stream)))
-     (format stream ".ELE ~A~%"
-             (model:calc-node-dir node :join-by *dir-delim*)))
+       (unless bogus-container
+         (format stream ".EL ~A ~A~%"
+                 (model:get-elem-children-num node)
+                 (model:calc-node-dir node :join-by *dir-delim*))
+         (with-truly namespace-uri (model:get-elem-namespace-uri node)
+           (format stream ".EL.NS ~A~%" (model:write-uri namespace-uri)))
+         (with-truly local-name (model:get-elem-local-name node)
+           (format stream ".EL.LOC.NAME ~A~%" local-name))
+         (with-truly qname (model:get-elem-qname node)
+           (format stream ".EL.QNAME ~A~%" qname))
+         (with-truly prefix-mappings (model:get-elem-prefix-mappings node)
+           (serialize-prefix-mappings doc node prefix-mappings stream))
+         (with-truly attributes (model:get-elem-attributes node)
+           (serialize-elem-attributes doc attributes stream)))
+
+       (with-truly children (model:get-elem-children node)
+         (dolist (child children)
+           (serialize-nodes doc child stream nil)))
+       (unless bogus-container
+         (format stream ".ELE ~A~%"
+                 (model:calc-node-dir node :join-by *dir-delim*))))
 
     (model:text
-     (with-truly content (model:get-text-content node)
-       (format stream ".TEXT~%~A~%.TEXTE~%" content)))
+       (with-truly content (model:get-text-content node)
+         (format stream ".TEXT~%~A~%.TEXTE~%" content)))
 
     (model:comment
-     (with-truly content (model:get-comment-content node)
-       (format stream ".COM~%~A~%.COME~%" content)))
+       (with-truly content (model:get-comment-content node)
+         (format stream ".COM~%~A~%.COME~%" content)))
 
     (model:cdata
-     (with-truly content (model:get-cdata-content node)
-       (format stream ".CD~%~A~%.CDE~%" content)))
+       (with-truly content (model:get-cdata-content node)
+         (format stream ".CD~%~A~%.CDE~%" content)))
 
     (model:pinstr
-     (with-truly target (model:get-pinstr-target node)
-       (format stream ".PI.TARG ~A~%" target))
-     (with-truly content (model:get-pinstr-data node)
-       (format stream ".PI~%~A~%.PIE~%" content)))))
+       (with-truly target (model:get-pinstr-target node)
+         (format stream ".PI.TARG ~A~%" target))
+       (with-truly content (model:get-pinstr-data node)
+         (format stream ".PI~%~A~%.PIE~%" content)))))
 
 
 (defun deserialize (in-stream)
